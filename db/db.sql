8cb3fb7dbf5c21896fa333fdc03a948127cbf4ad
Sujunghyun <whale14412@gmail.com>
2018-10-13 23:55:32 +0900

    181013 개발

--- db/db.sql
+++ db/db.sql
@@ -1,94 +1,97 @@
 -- 수정하기. 캘린더에 유저식별키 필요
 
 CREATE TABLE USER (    -- 사용자 테이블
-    U_ID VARCHAR(30),                       -- 로그인이나 회원가입 시에 연동 API에서 발급되는 토큰
-    U_NAME VARCHAR(20) NOT NULL,            -- 사용자명 (입력받는 값)
-    DEPARTMENT VARCHAR(30) NOT NULL,        -- 학과 (입력받는 값)
-    STUDENT_ID INT NOT NULL,                -- 학번 (학생만 입력받는 값)
-    GRADE TINYINT NOT NULL,                 -- 학년 (학생만 입력받는 값)
-    PHOTO_URL VARCHAR(100),                 -- 사진 URL. 토큰에 포함되어있는 값.
-    U_ROLE VARCHAR(10) NOT NULL,            -- 교수/학생 구분
-    PRIMARY KEY (U_ID)                      -- 기본키. 토큰
+    u_id VARCHAR(30),                       -- 로그인이나 회원가입 시에 연동 API에서 발급되는 토큰
+    u_name VARCHAR(20) NOT NULL,            -- 사용자명 (입력받는 값)
+    department VARCHAR(30) NOT NULL,        -- 학과 (입력받는 값)
+    student_id INT,                         -- 학번 (학생만 입력받는 값)
+    grade TINYINT,                          -- 학년 (학생만 입력받는 값)
+    photo_url VARCHAR(100),                 -- 사진 URL. 토큰에 포함되어있는 값.
+    u_role VARCHAR(10) NOT NULL,            -- 교수/학생 구분
+    PRIMARY KEY (u_id)                      -- 기본키. 토큰
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE LECTURE (    -- 강의 테이블
-    L_ID SMALLINT AUTO_INCREMENT,                -- 강의 일련번호
-    L_NAME VARCHAR(30) NOT NULL,                 -- 강의명 (개설 시 입력받는 값)
-    L_ROOM VARCHAR(6) NOT NULL,                  -- 강의실 (개설 시 입력받는 값)
-    L_GRADE TINYINT NOT NULL,                    -- 학년 (개설 시 입력받는 값)
-    PROF_NAME VARCHAR(20) NOT NULL,              -- 교수이름 (개설 시 입력받는 값)
-    CLASS VARCHAR(2) NOT NULL,                   -- 분반 (개설 시 입력받는 값)
-    START_TIME TIME,                             -- 강의 시작시간 (개설 시 입력받는 값)
-    END_TIME TIME,                               -- 강의 종료시간 (개설 시 입력받는 값)
-    SUPPLEMENT TINYINT(1) NOT NULL DEFAULT 0,    -- 보강여부 DEFAULT 값은 0(보강아님)
-    PRIMARY KEY (L_ID)                           -- 기본키. 강의 일련번호
+    l_id SMALLINT AUTO_INCREMENT,                -- 강의 일련번호
+    l_name VARCHAR(30) NOT NULL,                 -- 강의명 (개설 시 입력받는 값)
+    l_room VARCHAR(6) NOT NULL,                  -- 강의실 (개설 시 입력받는 값)
+    l_grade TINYINT NOT NULL,                    -- 학년 (개설 시 입력받는 값)
+    l_semester TINYINT NOT NULL,                 -- 학기 (개설 시 입력받는 값)
+    l_day VARCHAR(5) NOT NULL,                   -- 강의 요일 (개설 시 입력받는 값)
+    prof_name VARCHAR(20) NOT NULL,              -- 교수이름 (개설 시 입력받는 값)
+    class VARCHAR(2) NOT NULL,                   -- 분반 (개설 시 입력받는 값)
+    start_time TIME,                             -- 강의 시작시간 (개설 시 입력받는 값)
+    end_time TIME,                               -- 강의 종료시간 (개설 시 입력받는 값)
+    supplement TINYINT(1) DEFAULT 0,             -- 보강여부 DEFAULT 값은 0(보강아님)
+    PRIMARY KEY (l_id)                           -- 기본키. 강의 일련번호
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE COURSE (    -- 수강 테이블
-    C_ID INT AUTO_INCREMENT,                -- 수강 일련번호 
-    U_ID VARCHAR(30),                      
-    U_NAME VARCHAR(20) NOT NULL,            -- 사용자명
-    STUDENT_ID INT NOT NULL,                -- 학번
-    L_ID SMALLINT,                         
-    L_NAME VARCHAR(30) NOT NULL,            -- 강의명 (개설 시 입력받는 값)
-    START_TIME TIME,                        -- 강의 시작시간 (개설 시 입력받는 값)
-    END_TIME TIME,                          -- 강의 종료시간 (개설 시 입력받는 값)    
-    PRIMARY KEY (C_ID),                     -- 기본키. 수강 일련번호
-    FOREIGN KEY (U_ID)                      -- 외래키. 토큰
-    REFERENCES USER(U_ID),
-    FOREIGN KEY (L_ID)                      -- 외래키. 강의 일련번호
-    REFERENCES LECTURE(L_ID)
+    c_id INT AUTO_INCREMENT,                -- 수강 일련번호 
+    u_id VARCHAR(30),                      
+    u_name VARCHAR(20) NOT NULL,            -- 사용자명
+    student_id INT NOT NULL,                -- 학번
+    l_id SMALLINT,                         
+    l_name VARCHAR(30) NOT NULL,            -- 강의명 (개설 시 입력받는 값)
+    start_time TIME,                        -- 강의 시작시간 (개설 시 입력받는 값)
+    end_time TIME,                          -- 강의 종료시간 (개설 시 입력받는 값)    
+    beacon_id VARCHAR(50),                  -- 비콘 아이디
+    PRIMARY KEY (c_id),                     -- 기본키. 수강 일련번호
+    FOREIGN KEY (u_id)                      -- 외래키. 토큰
+    REFERENCES USER(u_id),
+    FOREIGN KEY (l_id)                      -- 외래키. 강의 일련번호
+    REFERENCES LECTURE(l_id)
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE ATTENDENCE (    -- 출결 테이블
-    A_ID INT AUTO_INCREMENT,                 -- 출결 일련번호
-    A_DATE DATE NOT NULL,                    -- 강의 날짜
-    C_ID INT,                                -- 수강정보 조회 시 필요.
-    ATTEND VARCHAR(20) NOT NULL,             -- 출결여부 (출석, 지각, 결석, 출결기타(병결, 조퇴, 휴강, etc...))
-    DEPART TINYINT(1) NOT NULL DEFAULT 0,    -- 이탈여부 DEFAULT 값은 0(이탈안함)
-    PRIMARY KEY (A_ID),                      -- 기본키. 출결 일련번호
-    FOREIGN KEY (C_ID)                       -- 외래키. 수강 일련번호
-    REFERENCES COURSE(C_ID)
+CREATE TABLE ATTENDENCE (    -- 출결 테이블 // 수강일련번호로 유저정보 조회해서 가져올 것.
+    a_id INT AUTO_INCREMENT,                 -- 출결 일련번호
+    a_date DATE NOT NULL,                    -- 강의 날짜
+    c_id INT,                                -- 수강정보 조회 시 필요.
+    attend VARCHAR(20) NOT NULL,             -- 출결여부 (출석, 지각, 결석, 출결기타(병결, 조퇴, 휴강, etc...))
+    depart TINYINT(1) DEFAULT 0,             -- 이탈여부 DEFAULT 값은 0(이탈안함), TINYINT(1)은 BOOLEAN 값을 표현.
+    PRIMARY KEY (a_id),                      -- 기본키. 출결 일련번호
+    FOREIGN KEY (c_id)                       -- 외래키. 수강 일련번호
+    REFERENCES COURSE(c_id)
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE BOARD (    -- 게시판 테이블
-    B_ID INT AUTO_INCREMENT,               -- 게시글 일련번호.
-    U_ID VARCHAR(30),  
-    U_NAME VARCHAR(20) NOT NULL,           -- U_ID 받아서 해결함.
-    DEPARTMENT VARCHAR(30) NOT NULL,       -- U_ID 받아서 해결함.
-    TITLE VARCHAR(30) NOT NULL,            -- 제목
-    CONTENTS VARCHAR(1000) NOT NULL,       -- 내용 
-    COUNTS SMALLINT NOT NULL,              -- 조회수
-    REG_DATE TIMESTAMP NOT NULL,           -- 작성일시로 정렬
-    COMMENTS SMALLINT NOT NULL,            -- 댓글수
-    PRIMARY KEY (B_ID),                    -- 기본키. 게시글 일련번호
-    FOREIGN KEY (U_ID)                     -- 외래키. 토큰
-    REFERENCES USER(U_ID)    
+    b_id INT AUTO_INCREMENT,               -- 게시글 일련번호.
+    u_id VARCHAR(30),  
+    u_name VARCHAR(20) NOT NULL,           -- U_ID 받아서 해결함.
+    department VARCHAR(30) NOT NULL,       -- U_ID 받아서 해결함.
+    title VARCHAR(30) NOT NULL,            -- 제목
+    contents VARCHAR(1000) NOT NULL,       -- 내용 
+    counts SMALLINT NOT NULL,              -- 조회수
+    reg_date TIMESTAMP NOT NULL,           -- 작성일시로 정렬
+    comments SMALLINT NOT NULL,            -- 댓글수
+    PRIMARY KEY (b_id),                    -- 기본키. 게시글 일련번호
+    FOREIGN KEY (u_id)                     -- 외래키. 토큰
+    REFERENCES USER(u_id)    
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE COMMENTS (    -- 게시판 댓글 테이블
-    CM_ID INT AUTO_INCREMENT,              -- 댓글 일련번호. 댓글을 찾아서 여기에 댓글을 등록할 수 있도록함.
-    B_ID INT,                              -- 게시글 일련번호. 어떤 글의 댓글인지 구분하기 위해 필요
-    U_ID VARCHAR(30),  
-    U_NAME VARCHAR(20) NOT NULL,           -- U_ID 받아서 해결함.
-    CONTENTS VARCHAR(300) NOT NULL,        -- 댓글 내용    
-    REG_DATE TIMESTAMP NOT NULL,           -- 작성일시로 정렬
-    DEPTH TINYINT NOT NULL,                -- 몇 차 댓글인지(댓글, 댓글의 댓글, ...)
-    PRIMARY KEY (CM_ID),                   -- 기본키. 댓글 일련번호
-    FOREIGN KEY (U_ID)                     -- 외래키. 토큰
-    REFERENCES USER(U_ID),
-    FOREIGN KEY (B_ID)                     -- 외래키. 게시글 일련번호 
-    REFERENCES BOARD(B_ID)
+    cm_id INT AUTO_INCREMENT,              -- 댓글 일련번호. 댓글을 찾아서 여기에 댓글을 등록할 수 있도록함.
+    b_id INT,                              -- 게시글 일련번호. 어떤 글의 댓글인지 구분하기 위해 필요
+    u_id VARCHAR(30),  
+    u_name VARCHAR(20) NOT NULL,           -- U_ID 받아서 해결함.
+    contents VARCHAR(300) NOT NULL,        -- 댓글 내용    
+    reg_date TIMESTAMP NOT NULL,           -- 작성일시로 정렬
+    depth TINYINT NOT NULL,                -- 몇 차 댓글인지(댓글, 댓글의 댓글, ...)
+    PRIMARY KEY (cm_id),                   -- 기본키. 댓글 일련번호
+    FOREIGN KEY (u_id)                     -- 외래키. 토큰
+    REFERENCES USER(u_id),
+    FOREIGN KEY (b_id)                     -- 외래키. 게시글 일련번호 
+    REFERENCES BOARD(b_id)
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE CALENDAR (    -- 캘린더 테이블
-    U_ID VARCHAR(30),    
-    DDATE DATE NOT NULL,                    -- 일정 년/월/일
-    START_TIME TIME,                        -- 일정 시작시간
-    END_TIME TIME,                          -- 일정 종료시간
-    TITLE VARCHAR(50) NOT NULL,             -- 일정 제목
-    PLACE VARCHAR(50),                      -- 일정 장소
-    SCHEDULE VARCHAR(200),                  -- 일정 내용
-    FOREIGN KEY (U_ID)                      -- 외래키. 토큰
-    REFERENCES USER(U_ID)
+    u_id VARCHAR(30),    
+    ddate DATE NOT NULL,                    -- 일정 년/월/일
+    start_time TIME,                        -- 일정 시작시간
+    end_time TIME,                          -- 일정 종료시간
+    title VARCHAR(50) NOT NULL,             -- 일정 제목
+    place VARCHAR(50),                      -- 일정 장소
+    schedule VARCHAR(200),                  -- 일정 내용
+    FOREIGN KEY (u_id)                      -- 외래키. 토큰
+    REFERENCES USER(u_id)
 ) ENGINE=innoDB DEFAULT CHARSET=utf8; 
--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,20 @@
+// 완료(A), 일단완료(B)  전체적으로 인증로직, 에러처리 보완필요, promise 적용, 리팩토링
+
 'use strict';
 
 const express = require('express');
 const router = express.Router();
 const pool = require('../db/db_pool')
 const _ = require('lodash');
+const moment = require('moment');
 
 // uid랑 email받아서 email을 id처럼 사용. 
 // uid를 facebook이나 google에 보내서 정상적인 사용자인지 확인
+
+// 로그인(B)
 router.post('/login', (req, res) => {
     let uid = req.body.uid,
-        sql = 'select * from USER where U_ID = ?';     
+        sql = 'select * from USER where u_id = ?';     
     pool.getConnection(function(err, connection) {
         if (err) throw err;        
         var query = connection.query(sql, uid, function(err, rows) {
@@ -35,14 +40,24 @@ router.post('/login', (req, res) => {
     });
 });
 
+// 회원가입(B)
 router.post('/signup', (req, res) => { 
-    // _.pick로 파라미터 정리
-    const temp = _.pick(req.body, ['uid', 'uname', 'department', 'student_id', 'grade', 'photo_url', 'role']),
-          sql = 'insert into USER (U_ID, U_NAME, DEPARTMENT, STUDENT_ID, GRADE, PHOTO_URL, U_ROLE) values (?,?,?,?,?,?,?);'
+    let role = req.body.role;
+    console.log(role);
+    
+    // 학생일 경우, 학번과 학년을 추가적으로 받는다.
+    if (role == 'stu') {
+        /* 제어문 바깥으로 변수값을 가져가기 위해(스코프 조정을 위해) 여기서만 변수를 var로 선언한다.
+           _.pick로 파라미터 정리 */
+        var temp = _.pick(req.body, ['uid', 'uname', 'department', 'student_id', 'grade', 'photo_url', 'role']),
+        sql = 'insert into USER (u_id, u_name, department, student_id, grade, photo_url, u_role) values (?,?,?,?,?,?,?);' 
+    } else {
+        var temp = _.pick(req.body, ['uid', 'uname', 'department', 'photo_url', 'role']),
+        sql = 'insert into USER (u_id, u_name, department, photo_url, u_role) values (?,?,?,?,?);'    
+    }
     // _.toArray로 타입을 Array로 변경. query 시 필요
     const params = _.toArray(temp);
     console.log(params);
-    
     pool.getConnection(function(err, connection) {
         if (err) throw err;
         var query = connection.query(sql, params, function(err, rows) {
@@ -62,5 +77,235 @@ router.post('/signup', (req, res) => {
     });
 });
 
+// 강의개설(교수)(B)
+router.post('/openlecture', (req, res) => {
+    // _.pick로 파라미터 정리
+    const temp = _.pick(req.body, ['l_name', 'l_room', 'l_grade', 'l_semester', 'l_day', 'prof_name', 'class', 'start_time', 'end_time', 'supplement']),
+          sql = 'insert into LECTURE (l_name, l_room, l_grade, l_semester, l_day, prof_name, class, start_time, end_time, supplement) values (?,?,?,?,?,?,?,?,?,?);'
+    // _.toArray로 타입을 Array로 변경. query 시 필요
+    const params = _.toArray(temp);
+    console.log(params);
+    
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        var query = connection.query(sql, params, function(err, rows) {
+            if (err) {
+                connection.release();
+                throw err;   
+            } else {
+                if (rows.length === 0) {
+                    res.status(400).json({false: '강의개설에 실패하였습니다.'});
+                    connection.release();
+                } else {
+                    res.status(200).json({success: '강의개설에 성공하였습니다.'});
+                    connection.release();
+                }
+            }
+        });
+    });    
+});
+
+// 개설 강의 조회(학생)(B) 
+router.get('/checklecture/:l_grade/:l_semester', (req, res) => {
+    // _.pick로 파라미터 정리
+    const temp = _.pick(req.params, ['l_grade', 'l_semester']),
+          sql = 'select * from LECTURE where l_grade = ? and l_semester = ?;'    
+    // _.toArray로 타입을 Array로 변경. query 시 필요
+    const params = _.toArray(temp);
+    console.log(params);
+    
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        var query = connection.query(sql, params, function(err, rows) {
+            if (err) {
+                connection.release();
+                throw err;   
+            } else {
+                if (rows.length === 0) {
+                    res.status(400).json({false: '개설된 강의가 없습니다.'});
+                    connection.release();
+                } else {
+                    res.status(200).json(rows);   // 개설 강의 리스트
+                    connection.release();
+                }
+            }
+        });
+    });    
+});
+
+// 수강신청(학생)(B)
+router.post('/register', (req, res) => {
+    const temp = _.pick(req.body, ['u_id', 'l_id']),
+          select_sql = 'select u_name, student_id, l_name, start_time, end_time from USER, LECTURE where  u_id = ? and l_id = ?;',
+          insert_sql = 'insert into COURSE (u_id, l_id, u_name, student_id, l_name, start_time, end_time) values (?,?,?,?,?,?,?);'
+    const params1 = _.toArray(temp);
+
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        var select = connection.query(select_sql, params1, function(err, rows) {
+            if (err) {
+                connection.release();
+                throw err;   
+            } else {
+                if (rows.length === 0) {
+                    res.status(400).json({false: '강의 조회에 실패하였습니다.'});  // u_id or l_id 오류
+                    connection.release();
+                } else {                    
+                    const params2 = _.toArray(rows[0]);
+                    const params3 = [...params1, ...params2];
+                    console.log(params3);
+                    var insert = connection.query(insert_sql, params3, function(err, rows2) {
+                        if (err) {
+                            connection.release();
+                            throw err;   
+                        } else {
+                            if (rows2.length === 0) {
+                                res.status(400).json({false: '수강신청에 실패하였습니다.'});  // params3 오류
+                                connection.release();
+                            } else {
+                                res.status(200).json({success: '수강신청에 성공하였습니다.'});
+                                connection.release();
+                            }
+                        }
+                    });                    
+                }
+            }
+        });
+    });    
+});
+
+// 강의정보 조회(교수)(B)
+router.get('/courseinfo/:l_id/', (req, res) => {
+    // _.pick로 파라미터 정리
+    const l_id = req.params.l_id,
+          sql = 'select * from LECTURE where l_id = ?;'
+    
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        var query = connection.query(sql, l_id, function(err, rows) {
+            if (err) {
+                connection.release();
+                throw err;   
+            } else {
+                if (rows.length === 0) {
+                    res.status(400).json({false: '강의정보 조회에 실패했습니다.'});
+                    connection.release();
+                } else {
+                    res.status(200).json(rows[0]);   // 일련번호로 검색한 강의의 모든 정보를 반환
+                    connection.release();
+                }
+            }
+        });
+    });    
+});
+
+// 출석부 조회(교수)(B) - 해당 강의를 듣는 학생의 리스트를 조회한다.
+router.get('/rollbook/:c_id/', (req, res) => {
+    // _.pick로 파라미터 정리
+    const c_id = req.params.c_id,
+          sql = 'select student_id, u_name from COURSE where c_id = ?;'
+    
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        var query = connection.query(sql, c_id, function(err, rows) {
+            if (err) {
+                connection.release();
+                throw err;   
+            } else {
+                if (rows.length === 0) {
+                    res.status(400).json({false: '강의정보 조회에 실패했습니다.'});
+                    connection.release();
+                } else {
+                    res.status(200).json(rows);   // 해당 강의를 듣는 학생들의 학번과 이름을 반환
+                    connection.release();
+                }
+            }
+        });
+    });    
+});
+
+// beacon 등록/수정 기능(교수)(B)
+router.put('/reg_beacon', (req, res) => {
+    const beacon_id = req.body.beacon_id,
+          sql = 'update COURSE set beacon_id = ?;'
+    
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        var query = connection.query(sql, beacon_id, function(err, rows) {
+            if (err) {
+                connection.release();
+                throw err;   
+            } else {
+                if (rows.length === 0) {
+                    res.status(400).json({false: 'beacon 등록에 실패하였습니다.'});
+                    connection.release();
+                } else {
+                    res.status(200).json({success: 'beacon 등록에 성공하였습니다.'});
+                    connection.release();
+                }
+            }
+        });
+    });    
+});
+
+
+/* 
+    1. 강의 테이블에 강의 상태 속성 추가
+    2. 수업 시작/종료 기능
+    3. 수업 시작 종료를 기준으로 출석 기능 재구성
+    4. 해당 강의 상태 가져오기 (한 학생이 수강하는 모든 강의인지 아니면 한 개 강의인지 선우형한테 물어보기)
+    5. 강의 화면 - 출결 수정
+    6. 교수 출결 통계 화면 - 출결 수정
+    7. 교수/학생 - 출결 통계 확인
+    8. 푸시 기능
+*/
+
+// 출석(학생)(B)  -- 교수가 수업 시작하면, 출석 시작. 그 전까지는 이 요청이 올 경우 출석 대기 중.
+router.post('/attendance', (req, res) => { 
+    const c_id = req.body.c_id,
+          beacon_id = req.body.beacon_id,
+          tmpTime1 = new Date(),
+          tmpTime2 = moment(tmpTime1).add(9, 'hours'),
+          tmpTime3 = tmpTime2.toISOString(),
+          currentDate = tmpTime3.substring(0, 10),       // 오늘 날짜를 ISO 형식 날짜로 표현한다.
+          currentTime = tmpTime3.substring(11, 19),      // 현재 시간을 ISO 형식 날짜로 표현한다.
+          select_sql = 'select start_time, end_time, beacon_id from COURSE where c_id = ?',
+          insert_sql = 'insert into ATTENDENCE (a_date, c_id, attend) values (?,?,?);',
+          params = [currentDate, c_id];
+    let attend;
+        
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        var query = connection.query(select_sql, c_id, function(err, rows) {
+            if (err) {
+                connection.release();
+                throw err;   
+            } else {
+                if (rows.length === 0) {
+                    res.status(400).json({false: '잘못된 수강ID 입니다.'});
+                    connection.release();
+                } else {
+                    if (beacon_id === rows[0].beacon_id) {  // 비콘아이디가 일치하는지(올바른 강의에 출석하는지) 확인 
+                        if (rows[0].start_time >= currentTime) {    // 시작시간보다 출석이 이르거나 같다면 
+                            attend = '출석';
+                        } else if (rows[0].end_time < currentTime) {    // 종료시간이 지나도 출석이 없으면
+                            attend = '결석';
+                        } else {    // 둘 다 아니라면 즉, 시작시간과 종료시간 사이면 
+                            attend = '지각';
+                        }
+                        params.push(attend);
+                        var query = connection.query(insert_sql, params, function(err, rows2) {
+                            res.status(200).json({success: '출결처리를 성공하였습니다.'});
+                            connection.release();
+                        });
+                    } else {
+                        res.status(400).json({false: '잘못된 비콘ID 입니다.'});
+                        connection.release();
+                    }               
+                }
+            }
+        });
+    });
+});
 
 module.exports = router;
