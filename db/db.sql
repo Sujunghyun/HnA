3c50a12079d4dadd88ceff858186e8931b52e192
Sujunghyun <whale14412@gmail.com>
2018-10-16 23:30:04 +0900

    table USER, LECTURE, ATTANDANCE, BOARD 수정

--- db/db.sql
+++ db/db.sql
@@ -3,8 +3,8 @@
 CREATE TABLE USER (    -- 사용자 테이블
     u_id VARCHAR(30),                       -- 로그인이나 회원가입 시에 연동 API에서 발급되는 토큰
     u_name VARCHAR(20) NOT NULL,            -- 사용자명 (입력받는 값)
-    department VARCHAR(30) NOT NULL,        -- 학과 (입력받는 값)
-    student_id INT,                         -- 학번 (학생만 입력받는 값)
+    u_depart VARCHAR(30) NOT NULL,          -- 학과 (입력받는 값)
+    identifier VARCHAR(20) NOT NULL,        -- 학번/사번 (입력받는 값)
     grade TINYINT,                          -- 학년 (학생만 입력받는 값)
     photo_url VARCHAR(100),                 -- 사진 URL. 토큰에 포함되어있는 값.
     u_role VARCHAR(10) NOT NULL,            -- 교수/학생 구분
@@ -18,8 +18,8 @@ CREATE TABLE LECTURE (    -- 강의 테이블
     l_grade TINYINT NOT NULL,                    -- 학년 (개설 시 입력받는 값)
     l_semester TINYINT NOT NULL,                 -- 학기 (개설 시 입력받는 값)
     l_day VARCHAR(5) NOT NULL,                   -- 강의 요일 (개설 시 입력받는 값)
+    l_class VARCHAR(2) NOT NULL,                 -- 분반 (개설 시 입력받는 값)
     prof_name VARCHAR(20) NOT NULL,              -- 교수이름 (개설 시 입력받는 값)
-    class VARCHAR(2) NOT NULL,                   -- 분반 (개설 시 입력받는 값)
     start_time TIME,                             -- 강의 시작시간 (개설 시 입력받는 값)
     end_time TIME,                               -- 강의 종료시간 (개설 시 입력받는 값)
     supplement TINYINT(1) DEFAULT 0,             -- 보강여부 DEFAULT 값은 0(보강아님)
@@ -30,7 +30,7 @@ CREATE TABLE COURSE (    -- 수강 테이블
     c_id INT AUTO_INCREMENT,                    -- 수강 일련번호 
     u_id VARCHAR(30),                      
     u_name VARCHAR(20) NOT NULL,                -- 사용자명
-    student_id INT NOT NULL,                    -- 학번
+    identifier VARCHAR(20) NOT NULL,            -- 학번/사번
     l_id SMALLINT,                         
     l_name VARCHAR(30) NOT NULL,                -- 강의명 (개설 시 입력받는 값)
     state VARCHAR(30) DEFAULT '수업 준비 중',    -- 강의 상태
@@ -45,7 +45,7 @@ CREATE TABLE COURSE (    -- 수강 테이블
     REFERENCES LECTURE(l_id)
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE ATTENDENCE (    -- 출결 테이블 // 수강일련번호로 유저정보 조회해서 가져올 것.
+CREATE TABLE ATTENDANCE (    -- 출결 테이블 // 수강일련번호로 유저정보 조회해서 가져올 것.
     a_id INT AUTO_INCREMENT,                 -- 출결 일련번호
     a_date DATE NOT NULL,                    -- 강의 날짜
     c_id INT,                                -- 수강정보 조회 시 필요.
@@ -60,7 +60,7 @@ CREATE TABLE BOARD (    -- 게시판 테이블
     b_id INT AUTO_INCREMENT,               -- 게시글 일련번호.
     u_id VARCHAR(30),  
     u_name VARCHAR(20) NOT NULL,           -- U_ID 받아서 해결함.
-    department VARCHAR(30) NOT NULL,       -- U_ID 받아서 해결함.
+    u_depart VARCHAR(30) NOT NULL,         -- U_ID 받아서 해결함.
     title VARCHAR(30) NOT NULL,            -- 제목
     contents VARCHAR(1000) NOT NULL,       -- 내용 
     counts SMALLINT NOT NULL,              -- 조회수
