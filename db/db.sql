cd4f5ee5ab730ed18fc571a9461e27b1023194fd
Sujunghyun <whale14412@gmail.com>
2018-10-17 23:56:59 +0900

    table USER, LECTURE, ATTENDANCE 수정

--- db/db.sql
+++ db/db.sql
@@ -6,7 +6,7 @@ CREATE TABLE USER (    -- 사용자 테이블
     u_depart VARCHAR(30) NOT NULL,          -- 학과 (입력받는 값)
     identifier VARCHAR(20) NOT NULL,        -- 학번/사번 (입력받는 값)
     grade TINYINT,                          -- 학년 (학생만 입력받는 값)
-    photo_url VARCHAR(100),                 -- 사진 URL. 토큰에 포함되어있는 값.
+    photo_url VARCHAR(1000),                -- 사진 URL. 토큰에 포함되어있는 값.
     u_role VARCHAR(10) NOT NULL,            -- 교수/학생 구분
     PRIMARY KEY (u_id)                      -- 기본키. 토큰
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
@@ -14,7 +14,7 @@ CREATE TABLE USER (    -- 사용자 테이블
 CREATE TABLE LECTURE (    -- 강의 테이블
     l_id SMALLINT AUTO_INCREMENT,                -- 강의 일련번호
     l_name VARCHAR(30) NOT NULL,                 -- 강의명 (개설 시 입력받는 값)
-    l_room VARCHAR(6) NOT NULL,                  -- 강의실 (개설 시 입력받는 값)
+    l_room VARCHAR(60) NOT NULL,                 -- 강의실 (개설 시 입력받는 값)
     l_grade TINYINT NOT NULL,                    -- 학년 (개설 시 입력받는 값)
     l_semester TINYINT NOT NULL,                 -- 학기 (개설 시 입력받는 값)
     l_day VARCHAR(5) NOT NULL,                   -- 강의 요일 (개설 시 입력받는 값)
@@ -47,7 +47,7 @@ CREATE TABLE COURSE (    -- 수강 테이블
 
 CREATE TABLE ATTENDANCE (    -- 출결 테이블 // 수강일련번호로 유저정보 조회해서 가져올 것.
     a_id INT AUTO_INCREMENT,                 -- 출결 일련번호
-    a_date DATE NOT NULL,                    -- 강의 날짜
+    a_date VARCHAR(10) NOT NULL,             -- 강의 날짜
     c_id INT,                                -- 수강정보 조회 시 필요.
     attend VARCHAR(20) NOT NULL,             -- 출결여부 (출석, 지각, 결석, 출결기타(병결, 조퇴, 휴강, etc...))
     depart TINYINT(1) DEFAULT 0,             -- 이탈여부 DEFAULT 값은 0(이탈안함), TINYINT(1)은 BOOLEAN 값을 표현.
