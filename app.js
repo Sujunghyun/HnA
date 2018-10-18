2bc37876733a40c0f7551a5ba4acabe8615838d8
Sujunghyun <whale14412@gmail.com>
2018-10-18 23:20:20 +0900

    에러처리 포맷 수정

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.use('/', router);  // router 사용설정은 bodyParser 사용설정 보다 
 
 //===== 404 처리 부분 =====//
 app.use((req, res, next) => {
-    res.status(404).send('일치하는 주소가 없습니다!');
+    res.status(404).json({ false: '일치하는 주소가 없습니다!'});
 });
 
 //===== 에러 처리 부분 =====//
 app.use((err, req, res, next) => {  
     console.error(err.stack); // 에러 메시지 표시
-    res.status(500).send('서버 에러!'); // 500 상태 표시 후 에러 메시지 전송
+    res.status(500).json({ false:'서버 에러!'}); // 500 상태 표시 후 에러 메시지 전송
 });
 
 //===== 서버 시작 =====//
