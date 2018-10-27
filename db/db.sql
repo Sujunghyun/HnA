0a056ed5e36797b9d481c42b101d259f23b9c065
Sujunghyun <whale14412@gmail.com>
2018-10-27 22:42:42 +0900

    게시판 관련 테이블 삭제 및 외래키 CASCADE 처리

--- db/db.sql
+++ db/db.sql
@@ -34,7 +34,7 @@ CREATE TABLE LECTURE_DT (    -- 강의 일자 테이블
     l_date  VARCHAR(10),                -- 강의 요일 (개설 시 입력받는 값)
     PRIMARY KEY (ld_id),                -- 기본키. 강의 일자 일련번호
     FOREIGN KEY (l_id)                  -- 외래키. 강의 일련번호
-    REFERENCES LECTURE(l_id)    
+    REFERENCES LECTURE(l_id) ON DELETE CASCADE   
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE COURSE (    -- 수강 테이블
@@ -52,18 +52,19 @@ CREATE TABLE COURSE (    -- 수강 테이블
     FOREIGN KEY (u_id)                          -- 외래키. 토큰
     REFERENCES USER(u_id),
     FOREIGN KEY (l_id)                          -- 외래키. 강의 일련번호
-    REFERENCES LECTURE(l_id)
+    REFERENCES LECTURE(l_id) ON DELETE CASCADE
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ATTENDANCE (    -- 출결 테이블 // 수강일련번호로 유저정보 조회해서 가져올 것.
     a_id INT AUTO_INCREMENT,                 -- 출결 일련번호
     a_date VARCHAR(10) NOT NULL,             -- 강의 날짜
-    c_id INT,                                -- 수강정보 조회 시 필요.
+    c_id INT,                                -- 수강정보 조회 시 필요.    
+    real_attend_time TIME,                   -- 출석 시간(학생이 실제로 출석 요청한 시간)
     attend VARCHAR(50) NOT NULL,             -- 출결여부 (출석, 지각, 결석, 출결기타(병결, 조퇴, 휴강, etc...))
     depart TINYINT(1) DEFAULT 0,             -- 이탈여부 DEFAULT 값은 0(이탈안함), TINYINT(1)은 BOOLEAN 값을 표현.
     PRIMARY KEY (a_id),                      -- 기본키. 출결 일련번호
     FOREIGN KEY (c_id)                       -- 외래키. 수강 일련번호
-    REFERENCES COURSE(c_id)
+    REFERENCES COURSE(c_id) ON DELETE CASCADE
 ) ENGINE=innoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE BOARD (    -- 게시판 테이블
@@ -105,5 +106,5 @@ CREATE TABLE CALENDAR (    -- 캘린더 테이블
     place VARCHAR(50),                      -- 일정 장소
     schedule VARCHAR(200),                  -- 일정 내용
     FOREIGN KEY (u_id)                      -- 외래키. 토큰
-    REFERENCES USER(u_id)
+    REFERENCES USER(u_id) ON DELETE CASCADE
 ) ENGINE=innoDB DEFAULT CHARSET=utf8; 
