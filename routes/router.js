816423a91eb0e01d04b39529e760ac37ec880573
Sujunghyun <whale14412@gmail.com>
2018-10-30 20:09:14 +0900

    출결통계(교수/학생) 수정, 수업 상태 변경 수정, 캘린더 관련 api 5개 추가 구현

--- routes/router.js
+++ routes/router.js
@@ -950,13 +950,26 @@ router.put('/lecture_state', (req, res) => {
                               connection.release();
                               throw err;
                             } else {
-                              if (absent1.length === 0) {  // 
-                                res.status(400).json({ false: '결석 처리를 위한 학생 조회를 실패하였습니다.' });    // 파라미터(l_id) 오류
-                                connection.release();
+                              attend = '결석';
+                              absenteeism.push(attend);
+                              if (absent1.length === 0) {  // 당일 강의에 학생이 한 명도 오지 않았을 경우. 학생 전원을 결석처리
+                                for (let i=0; i<rows2.length; i++) {
+                                  absenteeism.push(rows2[i].c_id);                                  
+                                  var absent_query = connection.query(ab_insert_sql, absenteeism, function (err, absent2) {
+                                    if (err) {
+                                      connection.release();
+                                      throw err;
+                                    } else {
+                                      if (absent2.length === 0) {
+                                        res.status(400).json({ false: '출석하지 않은 학생들의 처리를 실패하였습니다.' });
+                                        connection.release();
+                                      }
+                                    }
+                                  }); absenteeism.pop();
+                                }                                                                  
+                                res.status(200).json({ success: '수업 상태 변경을 성공하였습니다.' });  //  출석자가 1명도 없는 경우 - 수업 종료 시 수업 상태 종료로 변경
                               } else {    
-                                // 이탈 학생 결석 처리(완료)
-                                attend = '결석';
-                                absenteeism.push(attend);
+                                // 이탈 학생 결석 처리                                
                                 absent_params.push(l_id, state, currentDate);
                                 for (let i = 0; i<absent1.length; i++) {
                                   if (absent1[i].depart == 1) { // 수업 종료 시 이탈 중인 학생을 결석 처리
@@ -1418,7 +1431,7 @@ router.get('/prof_statistic/:l_id/:u_role', (req, res) => {
         function date (callback) {
           connection.query(date_sql, l_id, function (err, rows) {
             if (err) return callback(err);
-            if (rows.length == 0) return callback('통계 실패!!!!');
+            if (rows.length == 0) return callback(res.status(400).json({ false: '아직 수업하지 않은 강의입니다.' }));
             else if (rows) {
               for (let i=0; i<rows.length; i++) {
                 date_list.push(rows[i].l_date);
@@ -1431,7 +1444,7 @@ router.get('/prof_statistic/:l_id/:u_role', (req, res) => {
           for (let i=0; i<date_list.length; i++) {
             count_params.push(date_list[i]);
             connection.query(count_sql, count_params, function (err, rows) {
-              if (err) return callback(err);          
+              if (err) return callback(err);
               if (rows) count_list.push(rows[0]);         // cnt값 3개를 리스트에 넣음
               if (i == date_list.length-1) {
                 callback(null, date_list);      // 다음번 task에서 사용해야하는 값인 date_list를 callback 해줌.
@@ -1456,7 +1469,6 @@ router.get('/prof_statistic/:l_id/:u_role', (req, res) => {
         function (callback) {
           connection.query(select_stu_sql, l_id, function (err, rows) {
             if (err) return callback(err);
-            if (rows.length == 0) return callback('통계 실패!!!!');            
             else if (rows) {
               for (let i=0; i<rows.length; i++) {
                 stu_temp.push(rows[i]);                
@@ -1472,15 +1484,15 @@ router.get('/prof_statistic/:l_id/:u_role', (req, res) => {
       // Run task 
       async.waterfall(tasks, function (err) {
         if (err)
-          console.log(err);
+          console.log('err');
         else {
           // a_date & counts
           for (var i=0; i<date_list.length; i++) {
             let prof_elements = new prof_statistic(date_list[i], count_list[i].attend_cnt, count_list[i].lateness_cnt, count_list[i].absence_cnt);
             prof_stats.push(prof_elements)
           }
-          // s_list 
-          for (let k=0; k<stu_list.length; k++) {          
+          // s_list
+          for (let k=0; k<stu_list.length; k++) {       // 출석 데이터를 전부 수강데이터에 적용.
             for (let i=0; i<stu_list[0].length; i++) {
               for (let j=0; j<attend_list[0].length; j++) {
                 if (stu_list[k][i].identifier === attend_list[k][j].identifier) {
@@ -1497,7 +1509,7 @@ router.get('/prof_statistic/:l_id/:u_role', (req, res) => {
             prof_stats[i].s_list = stu_list[i];
           }
           console.log('done');                   
-          res.status(200).json(prof_stats);          
+          res.status(200).json(prof_stats);
           connection.release();
         }          
       });
@@ -1533,7 +1545,7 @@ router.get('/stu_statistic/:u_id/:u_role', (req, res) => {
     if (err) throw err;    
     if (u_role == 'stu') {
       var tasks = [
-        function (callback) {      // count 쿼리 잘못됨.
+        function (callback) {   
           connection.query(count_lesson_sql, u_id, function (err, rows) {
             if (err) return callback(err);
             if (rows.length == 0) return callback('통계 실패!!!!');
@@ -1599,6 +1611,9 @@ router.get('/stu_statistic/:u_id/:u_role', (req, res) => {
         else {
           // 학생의 전체 출결 통계
           stats_temp = (sum_cnt.attend_sum + sum_cnt.lateness_sum - (sum_cnt.lateness_sum/3)) / total_cnt.total;
+          if (isNaN(stats_temp)) {
+            stats_temp = 0;
+          }
           statistics = stats_temp*100 + '%';        
           // attend_list에서 시작시간 ~ 종료시간으로 포맷 만들 것
           for (let i=0; i<attend_list.length; i++) {
@@ -1629,6 +1644,205 @@ router.get('/stu_statistic/:u_id/:u_role', (req, res) => {
       res.status(400).json({ false: '잘못된 사용자 요청입니다.' });  // body(u_role) 오류
       connection.release();
     }
+  });
+});
+
+// 일정 등록(교수)(B)
+router.post('/schedule', (req, res) => {
+  console.log('일정 등록');
+  const temp = _.pick(req.body, ['u_id', 'u_depart', 'sc_day', 'start_date', 'sc_start_time', 'end_date', 'sc_end_time', 'title', 'place', 'schedule']),
+        params = _.toArray(temp), u_role = req.body.u_role,
+        insert_sql = 'insert into CALENDAR (u_id, u_depart, sc_day, start_date, sc_start_time, end_date, sc_end_time, title, place, schedule) values (?,?,?,?,?,?,?,?,?,?);';
+  let sc_id;
+
+  if (u_role === 'prof') {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;      
+      var tasks = [
+        function (callback) {    // 교수가 일정 등록
+          connection.query(insert_sql, params, function (err, rows) {
+            if (err) return callback(err);
+            if (rows.length == 0) {
+              return callback(res.status(400).json({ false: '일정 등록을 실패하였습니다.' }));  // body 오류
+            } else{
+              sc_id = rows.insertId;              
+              return callback(null);         
+            }
+          });
+        }
+      ];
+      // Run task 
+      async.waterfall(tasks, function (err) {
+        if (err)
+          console.log('err');
+        else {          
+          console.log('done');                   
+          res.status(200).json({sc_id, success: '일정을 등록했습니다.' });
+          connection.release();
+        }          
+      });
+    });
+  } else {
+    res.status(400).json({ false: '잘못된 사용자 요청입니다.' });  // body(u_role) 오류    
+  }
+});
+
+// 일정 조회(B)
+router.get('/schedule/:identifier/:sc_id', (req, res) => {
+  console.log('일정 조회');
+  const identifier = req.params.identifier, sc_id = req.params.sc_id, 
+        select_depart_sql1 = 'select u_depart from USER where identifier = ?;',
+        select_depart_sql2 = 'select u_depart from CALENDAR where sc_id = ?;',
+        select_schedule_sql = 'select sc_day, start_date, sc_start_time, end_date, sc_end_time, title, place, schedule from CALENDAR where sc_id = ?;';
+  let schedule_info, u_rows = {}, cl_rows = {};
+
+  pool.getConnection(function (err, connection) {
+    if (err) throw err;
+    var tasks = [
+      function (callback) {      // 일정을 수정하려는 교수가 일정을 등록한 교수와 같은 학과인지 확인 
+        connection.query(select_depart_sql1, identifier, function (err, rows) {
+          if (err) return callback(err);
+          if (rows.length == 0) {
+            return callback(res.status(400).json({ false: '잘못된 사용자의 요청입니다.' }));  // body 중 identifier 오류
+          } else {
+            u_rows.u_depart = rows[0].u_depart;
+            return callback(null);  
+          }
+        });
+      },
+      function (callback) {      // 일정을 수정하려는 교수가 일정을 등록한 교수와 같은 학과인지 확인 
+        connection.query(select_depart_sql2, sc_id, function (err, rows) {
+          if (err) return callback(err);
+          if (rows.length == 0) {
+            return callback(res.status(400).json({ false: '존재하지 않는 일정입니다.' }));  // body 중 sc_id 오류
+          } else {
+            cl_rows.u_depart = rows[0].u_depart;
+            return callback(null);  
+          }
+        });
+      },
+      function (callback) {     // 일정 조회
+        if (u_rows.u_depart == cl_rows.u_depart) {
+          connection.query(select_schedule_sql, sc_id, function (err, rows) {
+            if (err) return callback(err);
+            if (rows.length == 0) {
+              return callback(res.status(400).json({ false: '일정 조회에 실패했습니다.' }));  // sc_id 오류
+            } else{
+              schedule_info = rows[0];
+              return callback(null);         
+            }
+          });
+        } else {
+          return callback(res.status(400).json({ false: '다른 학과의 일정입니다.' }));
+        }        
+      }
+    ];
+    // Run task 
+    async.waterfall(tasks, function (err) {
+      if (err)
+        console.log(err);
+      else {          
+        console.log('done');                   
+        res.status(200).json(schedule_info);
+        connection.release();
+      }          
+    });
+  });
+});
+
+// 일정 수정(교수)(B)
+router.put('/schedule', (req, res) => {
+  console.log('일정 수정');
+  const temp = _.pick(req.body, ['u_id', 'u_depart', 'sc_day', 'start_date', 'sc_start_time', 'end_date', 'sc_end_time', 'title', 'place', 'schedule', 'sc_id']),
+        u_role = req.body.u_role, u_id = req.body.u_id, u_depart = req.body.u_depart, params = _.toArray(temp), 
+        select_sql = 'select u_depart from USER where u_id = ?;',
+        update_sql = 'update CALENDAR set u_id=?, u_depart=?, sc_day=?, start_date=?, sc_start_time=?, end_date=?, sc_end_time=?, title=?, place=?, schedule=? where sc_id = ?;';
+
+  if (u_role === 'prof') {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;      
+      var tasks = [
+        function (callback) {      // 일정을 수정하려는 교수가 일정을 등록한 교수와 같은 학과인지 확인 
+          connection.query(select_sql, u_id, function (err, rows) {
+            if (err) return callback(err);
+            if (rows[0].u_depart == u_depart) {
+              callback(null);
+            } else {
+              return callback(res.status(400).json({ false: '다른 학과의 일정입니다.' }));
+            }
+          });
+        },
+        function (callback) {     // 일정 수정
+          connection.query(update_sql, params, function (err, rows) {
+            if (err) return callback(err);
+            if (rows.length == 0) {
+              return callback(res.status(400).json({ false: '일정 수정에 실패했습니다.' }));  // body 오류
+            } else{
+              return callback(null);         
+            }
+          });
+        }
+      ];
+      // Run task 
+      async.waterfall(tasks, function (err) {
+        if (err)
+          console.log('err');
+        else {          
+          console.log('done');                   
+          res.status(200).json({ success: '일정을 수정했습니다.'});
+          connection.release();
+        }          
+      });
+    });
+  } else {
+    res.status(400).json({ false: '잘못된 사용자의 요청입니다.' });  // body(u_role) 오류    
+  }
+});
+
+// 일정 삭제(교수)(B)
+router.delete('/schedule', (req, res) => {
+  console.log('일정 삭제');
+  const u_role = req.headers.u_role, sc_id = req.headers.sc_id, 
+        delete_sql = 'delete from CALENDAR where sc_id = ?';   // 선택한 일정을 삭제
+
+  if (u_role === 'prof') {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      ordinaryQuery(connection, delete_sql, sc_id, function(err, rows) {  // 교수가 일정 등록
+        if (err) throw err;      
+        else {        
+          if (rows == 0) {
+            res.status(400).json({ false: '일정 삭제를 실패했습니다.' });
+          } else {
+            res.status(200).json({ success: '일정을 삭제했습니다.' });
+          }
+        }    
+      });
+    });
+  } else {
+    res.status(400).json({ false: '잘못된 사용자의 요청입니다.' });  // body(u_role) 오류
+  }
+});
+
+// 월간 일정 조회(B)
+router.get('/monthly_schedule/:month/:identifier', (req, res) => {
+  console.log('월간 일정 조회');
+  const start_date = req.params.month, identifier = req.params.identifier, 
+        select_sql = 'select sc_id, start_date, title from CALENDAR where start_date like ' + "'" + start_date + '%' + "'" + 
+                     ' and u_depart in (select u_depart from USER where identifier = ?);';    
+  
+  pool.getConnection(function (err, connection) {
+    if (err) throw err;
+    ordinaryQuery(connection, select_sql, identifier, function(err, rows) {  // 교수가 일정 등록
+      if (err) throw err;      
+      else {        
+        if (rows == 0) {
+          res.status(400).json({ false: '월간 일정 조회를 실패했습니다.' });
+        } else {
+          res.status(200).json(rows);
+        }
+      }    
+    });
   });
 });
 
@@ -1672,6 +1886,7 @@ class prof_statistic {
   }
 }
 
+// 학생 통계 데이터 포맷용 class
 class stu_statistic {
   constructor(l_name, l_attend_cnt, l_lateness_cnt, l_absence_cnt, a_list) {
     this.l_name = l_name;
@@ -1681,4 +1896,5 @@ class stu_statistic {
     this.a_list = a_list;
   }
 }
+
 module.exports = router;
